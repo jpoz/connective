772e63a73a432a67fdf3cffaae88640cfac9be06
JPoz <jpoz@jpoz.net>
2010-06-19 12:07:13 -0700

    adding get functionality

--- connective.js
+++ connective.js
@@ -1,12 +1,20 @@
 Connective = {
+  get:function(url, callback) {
+    var uuid = Connective._setup_uuid_callback();
+    Connective._callback_store[uuid] = new ConnectivePost(uuid, url, callback);
+  },
   connectTo: function(url, callback) {
-    var uuid = Connective._uuid();
-    Connective['receive_'+uuid] = new Function("json","Connective._callback_store["+uuid+"].receive(json)");    
+    var uuid = Connective._setup_uuid_callback();
     Connective._callback_store[uuid] = new ConnectiveConnection(uuid, url, callback);
   },
   
   // Private methods
   
+  _setup_uuid_callback: function() {
+    var uuid = Connective._uuid();
+    Connective['receive_'+uuid] = new Function("json","Connective._callback_store["+uuid+"].receive(json)");
+    return uuid;
+  },
   _ping:function(uuid){
     Connective._callback_store[uuid].ping()
   },
@@ -36,13 +44,25 @@ ConnectiveConnection.prototype.receive = function(json) {
 }
 
 ConnectiveConnection.prototype.ping = function() {
+  document.getElementsByTagName('head')[0].removeChild(this.script);
   this.connect();
 }
 
 ConnectiveConnection.prototype.connect = function() {
-  if (this.script) document.getElementsByTagName('head')[0].removeChild(this.script);
   this.script = document.createElement('script');
   this.script.setAttribute('onload',"Connective._ping("+this.uuid+");");
-  this.script.setAttribute('src', this.url + "?ts="+Connective._timestamp()+"&jsonp=Connective.receive_"+this.uuid);
+  this.script.setAttribute('src', this.url + "&ts="+Connective._timestamp()+"&jsonp=Connective.receive_"+this.uuid);
   document.getElementsByTagName('head')[0].appendChild(this.script);
 }
+
+function ConnectivePost(uuid, url, callback) {
+  this.uuid     = uuid;
+  this.url      = url
+  this.callback = callback
+  this.connect();
+  return this
+}
+ConnectivePost.prototype = new ConnectiveConnection();
+ConnectivePost.prototype.ping = function() {
+  document.getElementsByTagName('head')[0].removeChild(this.script);
+};
--- examples/server.js
+++ examples/server.js
@@ -1,17 +1,26 @@
 var http = require('http'),
     port = process.env.PORT || 3000
-    url_parse = require('url').parse,
+    url_parse = require('url').parse
+    sys = require('sys');
     
+var value    = "Awesome";
+var revision = 0 
+
+
 http.createServer(function (req, res) {
   var query = url_parse(req.url, true).query;
   if (query) {
-    if(query.jsonp) {
-      setTimeout(function() { 
-        res.writeHead(200, {'Content-Type':'text/javascript'});
-        res.end(query.jsonp+"(" + JSON.stringify({
-          value: (new Date).valueOf()
-        }) + ");");
-      }, 3000);
+    sys.puts(sys.inspect(query));
+    if(query.update) {
+      value = query.update
+      res.writeHead(200, {'Content-Type':'text/javascript'});
+      res.end(query.jsonp+"();");
+    } else if(query.jsonp) {
+      new CometConnection(res, query.jsonp, function() {
+        return value;
+      },function() {
+        return {value: value};
+      });
     } else {
       res.writeHead(200); res.end();
     }
@@ -19,3 +28,40 @@ http.createServer(function (req, res) {
     res.writeHead(200); res.end();
   }
 }).listen(parseInt(port));
+
+
+function CometConnection(res, jsonp, check, return_value) {
+  this.jsonp = jsonp;
+  this.res = res;
+  this.check = check;
+  this.return_value = return_value;
+  this.previous_value = this.check();
+  this.start_check(150);
+  this.start_fail_safe(30000);
+  return this;
+}
+
+CometConnection.prototype.respond_with = function(obj) {
+  clearTimeout(this.fail_safe);
+  clearInterval(this.check_interval);
+  this.res.writeHead(200, {'Content-Type':'text/javascript'});
+  this.res.end(this.jsonp+"(" + JSON.stringify(obj) + ");");
+  sys.puts("RESPOND: " +this.jsonp+"(" + JSON.stringify(obj) + ");")
+} 
+
+CometConnection.prototype.start_check = function(interval) {
+  var self = this;
+  this.check_interval = setInterval(function() { self.preform_check(); }, interval);
+}
+
+CometConnection.prototype.preform_check = function() {
+  var current_value = this.check();
+  if (this.previous_value != current_value) {
+    this.respond_with(this.return_value());
+  }
+}
+
+CometConnection.prototype.start_fail_safe = function(timeout) {
+  var self = this;
+  this.fail_safe = setTimeout(function() { self.respond_with(self.return_value()); }, timeout);
+}
